3dc41b37699ef4b8661f99cab5635b32abe69b0b
Chen <yun.chen.external@atos.net>
2019-11-19 15:24:22 +0800

    1

--- Sample/Minitest2.py
+++ Sample/Minitest2.py
@@ -1,1 +1,54 @@
+#coding:utf-8
 __author__ = 'a633335'
+
+import unittest
+from appium import webdriver
+import time,os
+
+class MyTests(unittest.TestCase):
+    # 测试开始前执行的方法，测试开始前的准备
+    def setUp(self):
+        desired_caps = {
+                'platformName': 'Android',
+                'platformVersion': '9',
+                'deviceName': 'a57c5aae',
+                'appPackage': 'com.tencent.mm',
+                'appActivity': 'com.tencent.mm.ui.LauncherUI',
+                'automationName': 'Uiautomator2',
+                # 'udid': 'a57c5aae',
+                # 'resetKeyboard': True,
+                'noReset': True,
+               "automationName": "uiautomator2"
+                }
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)  # 连接Appium
+        self.driver.implicitly_wait(8)
+
+    def test_miniprogram1(self, t=500):
+        """简单登录，注销测试"""
+        time.sleep(3)
+        window = self.driver.get_window_size()
+        x1 = window['width'] * 0.5  # 起始x坐标
+        y1 = window['height'] * 0.2  # y1坐标，滑动起始点
+        y2 = window['height'] * 0.7  # y2坐标，滑动末尾点
+        self.driver.swipe(x1,y1,x1,y2,t) # 页面下拉
+        self.driver.find_element_by_id('com.tencent.mm:id/ka').click() # 点击进入小程序
+        time.sleep(2)
+        ha_xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.webkit.WebView/android.widget.Button/android.view.View[3]/android.view.View[2]/android.view.View[3]/android.view.View[1]"
+        self.driver.find_element_by_xpath(ha_xpath).click()
+        time.sleep(2)
+        #enter_xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.webkit.WebView/android.widget.Button/android.view.View[3]/android.view.View[3]'
+        #self.driver.find_element_by_xpath(enter_xpath).click()
+        #signin_xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View[2]/android.view.View[2]/android.view.View'
+        #self.driver.find_element_by_xpath(signin_xpath).click() # 点击进入登录
+        #account_xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View[2]/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View'
+        #self.driver.find_element_by_xpath(account_xpath).click()
+        #passwd_xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View[2]/android.view.View[2]/android.view.View[1]/android.view.View/android.view.View'
+        #self.driver.find_element_by_xpath(passwd_xpath).send_keys("qwer1234.")
+        #login_xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View[3]'
+        #self.driver.find_element_by_xpath(login_xpath).click()
+    # 测试结束后执行的方法
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
